5007065e4db988c2fb4f20dc7b17982953f12003
The Oh Brothers <us@theohbrothers.com>
2017-04-10 00:22:04 +0800

    Update DisableAdaptiveSleepService.bat

--- DisableAdaptiveSleepService.bat
+++ DisableAdaptiveSleepService.bat
@@ -1,3 +1,3 @@
-:: Ensure to run this file as admin user / System for the Task you will create in Task Scheduler.
+:: Ensure to run this file as admin user / SYSTEM for the Task you will create in Task Scheduler.
 sc stop AdaptiveSleepService
 sc config AdaptiveSleepService start=demand
